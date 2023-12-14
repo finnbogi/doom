9198984893785e86112dc681c506b2c1b63e1949
Finnbogi Rútur <asgeirsson@gmail.com>
2023-12-15 00:12:48 +0100

    fix python template and static issues

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import os
 
 app = Flask(__name__)
@@ -9,6 +9,10 @@ def index():
     pod_ip = os.getenv('POD_IP', 'Unknown Pod IP')
 
     return render_template('index.html', pod_name=pod_name, pod_ip=pod_ip)
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
